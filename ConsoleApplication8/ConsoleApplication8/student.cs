1bbdb341fed16c979c8363dfb22256972ce2296d
Aigerim Seilbekova <Aigerim Seilbekova>
2016-02-13 11:05:11 +0600

    Lab1 class-student

--- ConsoleApplication8/ConsoleApplication8/student.cs
+++ ConsoleApplication8/ConsoleApplication8/student.cs
@@ -8,17 +8,17 @@ namespace ConsoleApplication8
 {
     class Student
         {
-        public string name, lastname, id, faculty;
-        public double gpa;
+            public string name, lastname, id, faculty;
+            public double gpa;
 
-        public Student(string name, string lastname, string id, string faculty, double gpa)
-            {
-                this.name = name;
-                this.lastname = lastname;
-                this.id = id;
-                this.faculty = faculty;
-                this.gpa = gpa;
-            }
+            public Student(string name, string lastname, string id, string faculty, double gpa)
+                {
+                    this.name = name;
+                    this.lastname = lastname;
+                    this.id = id;
+                    this.faculty = faculty;
+                    this.gpa = gpa;
+                }
 
         public override string ToString()
             {
@@ -27,13 +27,12 @@ namespace ConsoleApplication8
         }
 
     class Program
-    {
-        static void Main(string[] args)
         {
-            Student student1 = new Student("Aigerim", "Seiilbekova", "15BD02050", "FIT", 2.6);
-
-            Console.WriteLine(student1);
-            Console.ReadKey();
+             static void Main(string[] args)
+                {
+                Student student1 = new Student("Aigerim", "Seiilbekova", "15BD02050", "FIT", 2.6);
+                Console.WriteLine(student1);
+                Console.ReadKey();
         }
     }
 }
